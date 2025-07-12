3a6567fe077677942eb732fa68d6a96b6121a3eb
harryxu <harryzhxu@gmail.com>
2025-07-12 16:22:50 +0800

    Add fcitx module.

--- modules/fcitx.nix
+++ modules/fcitx.nix
@@ -1,12 +1,15 @@
-{config, pkgs, ...}:
+{ config, pkgs, ...}:
 {
   i18n.inputMethod = {
     type = "fcitx5";
     enable = true;
     fcitx5.addons = with pkgs; [
       fcitx5-gtk             # alternatively, kdePackages.fcitx5-qt
+      kdePackages.fcitx5-qt
       fcitx5-chinese-addons  # table input method support
       fcitx5-nord            # a color theme
+      rime-data
+      fcitx5-rime
     ];
   };
 }
